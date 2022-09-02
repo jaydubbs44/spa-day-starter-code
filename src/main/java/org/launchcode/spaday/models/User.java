d94ba01642c9ef7c1f7b4dff38c0ac7fdfbff051
Will Christen <christenwill58@gmail.com>
2022-09-01 20:26:54 -0400

    Studio complete but debugging

--- src/main/java/org/launchcode/spaday/models/User.java
+++ src/main/java/org/launchcode/spaday/models/User.java
@@ -1,19 +1,44 @@
 package org.launchcode.spaday.models;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 public class User {
+
+    @NotNull
+    @NotBlank
+    @Size(min = 5, max = 15)
     private String username;
+
+    @Email
     private String email;
+
+    @Size(min = 6)
     private String password;
+
+    @Size(min = 6)
+    private String verify;
 
     public User() {
 
     }
 
-    public User(String username, String email, String password) {
+    public User(String username, String email, String password, String verify) {
         this();
         this.username = username;
         this.email = email;
         this.password = password;
+        this.verify = verify;
+    }
+
+    public String getVerify() {
+        return verify;
+    }
+
+    public void setVerify(String verify) {
+        this.verify = verify;
     }
 
     public String getUsername() {
